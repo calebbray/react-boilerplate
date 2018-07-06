e0450b0464954566dbcc1d085cc8010dccf66ff8
Caleb Bray <cjbray23@gmail.com>
2018-07-06 10:28:32 -0700

    react boilerplate V 1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const htmlwpp = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new htmlwpp({
+      template: './public/index.html'
+    })
+  ]
+};
